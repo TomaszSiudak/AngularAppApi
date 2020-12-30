ab3945358abd3fa5485520df0bdc021cafe4789f
Tomasz Siudak <61911506+TomaszSiudak@users.noreply.github.com>
2020-12-30 18:32:15 +0100

    Create Card.cs

---
+++ Tests/Tests/Pages/PagesElements/Card.cs
@@ -0,0 +1,17 @@
+﻿using Framework.SeleniumWrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Pages.PagesElements
+{
+    public class Card
+    {
+        public WebElement Title { get; set; }
+        public WebElement Image { get; set; }
+        public WebElement Footer { get; set; }
+        public WebElement LikeBtn { get; set; }
+    }
+}
