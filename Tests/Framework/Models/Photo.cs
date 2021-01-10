eabf55ee8cbd12cd518786661a64c46fe9da6d6b
Tomasz Siudak <61911506+TomaszSiudak@users.noreply.github.com>
2021-01-10 13:11:42 +0100

    TestDataProvider

---
+++ Tests/Framework/Models/Photo.cs
@@ -0,0 +1,10 @@
+﻿namespace Framework.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public bool MainPhoto { get; set; }
+    }
+}
---
+++ Tests/TestDataProvider/Program.cs
@@ -0,0 +1,32 @@
+﻿using Framework.API;
+using Framework.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataProvider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Seed();
+        }
+
+        public static void Seed()
+        {
+            AuthorizationAPI authAPI = new AuthorizationAPI();
+            var data = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\SeedData.json");
+            var pets = JsonConvert.DeserializeObject<List<Pet>>(data);
+            foreach (var pet in pets)
+            {
+                authAPI.AddPet(pet);
+            }
+        }
+    }
+}
