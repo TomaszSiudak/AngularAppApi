bae152e267424f4cdf3d1724b2de3c4935da3673
Tomasz Siudak <61911506+TomaszSiudak@users.noreply.github.com>
2021-01-23 12:35:02 +0100

    ..

--- PetBookAPI/PetBookAPI/Controllers/PetsController.cs
+++ PetBookAPI/PetBookAPI/Controllers/PetsController.cs
@@ -12,7 +12,7 @@ using PetBookAPI.Model;
 
 namespace PetBookAPI.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class PetsController : ControllerBase
